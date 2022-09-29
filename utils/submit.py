2ff2a953fc0d57fe44f8bd776ca48b1e0f8f3c5a
Dongwoo-Im <iwd960510@gmail.com>
2022-09-29 16:52:30 +0900

    add comment

--- utils/submit.py
+++ utils/submit.py
@@ -14,12 +14,12 @@ if __name__ == '__main__':
         os.makedirs(args.dst, exist_ok=True)
     
     memory_dir = os.getcwd()
-    os.chdir(args.src)
+    os.chdir(args.src) # for using zipfile
     
     submission = zipfile.ZipFile("submission.zip", 'w')
     for path in os.listdir():
         if path.endswith('.png'):
-            new_path = path.split('_')[0].split('.')[0] + '.png'
+            new_path = path.split('_')[0].split('.')[0] + '.png' # new_path : delete basicsr suffix in path
             shutil.move(path, new_path)
             submission.write(new_path)
     submission.close()
