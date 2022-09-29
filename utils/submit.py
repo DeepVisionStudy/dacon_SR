779bb576709daff277e0a1778b7ed36c9559317f
Dongwoo-Im <iwd960510@gmail.com>
2022-09-29 16:43:59 +0900

    remove src_suffix & zip saved by absolute path

--- utils/submit.py
+++ utils/submit.py
@@ -6,19 +6,23 @@ import argparse
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--src", type=str, default='results_merge')
+    parser.add_argument("--src", type=str, default='HAT/results/HAT_Dacon_AdamW/visualization/test')
     parser.add_argument("--dst", type=str, default='results_submit')
     args = parser.parse_args()
 
     if not os.path.exists(args.dst):
         os.makedirs(args.dst, exist_ok=True)
     
+    memory_dir = os.getcwd()
     os.chdir(args.src)
     
     submission = zipfile.ZipFile("submission.zip", 'w')
     for path in os.listdir():
         if path.endswith('.png'):
-            submission.write(path)
+            new_path = path.split('_')[0].split('.')[0] + '.png'
+            shutil.move(path, new_path)
+            submission.write(new_path)
     submission.close()
-
-    shutil.move("submission.zip", "../results_submit/submission.zip")
+    
+    new_path_zip = os.path.join(memory_dir, args.dst, "submission.zip")
+    shutil.move("submission.zip", new_path_zip)
